138758e461aa4e4a1400c50cfc65c43fea9fe730
Yukito Tamura <104616235+y-tamura@users.noreply.github.com>
2025-08-07 18:42:42 +0900

    modify version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tamfunc',
-    version='0.5',
+    version='1.0',
     packages=find_packages(),
     install_requires=[
         'xarray',
